b6a5ca4c89ebaeb21a5b75e97fd9cf65c506d220
O.K.G. <inariwind@gmail.com>
2026-04-01 20:23:55 -0400

    refactor: delete log

--- src/utils/handleSubmit.tsx
+++ src/utils/handleSubmit.tsx
@@ -1,15 +1,15 @@
-'use server';
+"use server";
 
-import formValidation from '@utils/formValidation';
-import mailConfig from '@utils/mailConfig';
-import { JSDOM } from 'jsdom';
-import DOMPurify from 'dompurify';
+import formValidation from "@utils/formValidation";
+import mailConfig from "@utils/mailConfig";
+import { JSDOM } from "jsdom";
+import DOMPurify from "dompurify";
 
 export async function handleSubmit(formData: FormData) {
-  const fields = { dir: '', email: '', subject: '', content: '' };
+  const fields = { dir: "", email: "", subject: "", content: "" };
 
   const handleSanitation = (val: string) => {
-    const win = new JSDOM('').window;
+    const win = new JSDOM("").window;
     const purify = DOMPurify(win);
 
     return purify.sanitize(val);
@@ -18,15 +18,15 @@ export async function handleSubmit(formData: FormData) {
   Object.keys(fields).map(
     (key) =>
       (fields[key as keyof typeof fields] = handleSanitation(
-        formData.get(key) as string
-      ))
+        formData.get(key) as string,
+      )),
   );
 
   const { dir, email, subject, content } = fields;
   const { isValidated } = formValidation({ email, subject, content });
 
   if (!isValidated) {
-    return { status: '400' };
+    return { status: "400" };
   } else {
     const { env } = process;
     const { HOST_SUCCESS_RESPONSE } = env;
@@ -40,13 +40,13 @@ export async function handleSubmit(formData: FormData) {
     try {
       const { response } =
         (await transporter.sendMail(messageSendDetailsObject)) || {};
-console.log('xxx', HOST_SUCCESS_RESPONSE, response)
+
       if (response.includes(HOST_SUCCESS_RESPONSE as unknown as string)) {
-        return { status: '201' };
+        return { status: "201" };
       }
     } catch (nodeMailerRequestError) {
       console.error(nodeMailerRequestError);
-      return { status: '401' };
+      return { status: "401" };
     }
   }
 }
