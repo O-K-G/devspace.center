b343f9cf6f26096550405c06fb1987a69c98d2a1
O.K.G. <inariwind@gmail.com>
2026-04-03 12:18:26 -0400

    refactor: stage 2 of 2

--- src/constants/urls.tsx
+++ src/constants/urls.tsx
@@ -1,5 +1,5 @@
 export const URLs = {
-  gitHub: 'https://github.com/O-K-G/consolelog.me',
+  gitHub: 'https://github.com/O-K-G/devspace.center',
   linkedIn: 'https://www.linkedin.com/in/o-k-g',
   figmaAttribution: 'https://www.figma.com/community/file/1192903581929005722',
   ccbyLicense: 'https://creativecommons.org/licenses/by/4.0',
